7bfbd141a814f15c88e7741912856f7777a15b0c
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-01-25 23:27:44 +0530

    Add files via upload

---
+++ 25-01-2026/problem.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int marks;
+
+    scanf("%d",&marks);
+
+    if (marks >= 90 && marks <= 100) {
+        printf("Grade A");
+    } else if (marks >= 80 && marks < 90) {
+        printf("Grade B");
+    } else if (marks >= 60 && marks < 80) {
+        printf("Grade C");
+    } else if (marks >= 40 && marks < 60) {
+        printf("Grade D");
+    }else if (marks < 40) {
+        printf("Fail");
+    }else {
+        printf("Invalid Marks");
+    }
+
+    
+    return 0;
+}
