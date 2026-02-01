d5fe8f8a1861150d5ba80de9633b3f1cbde8e731
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-01 22:28:39 +0530

    Add files via upload

---
+++ 01-02-2026/problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+
+    scanf("%d %d",&a,&b);
+
+    if (a>b) {
+        printf("a is grater");
+    } else {
+        printf("b is grater");
+    }
+    return 0;
+}
