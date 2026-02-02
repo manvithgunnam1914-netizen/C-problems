cb38d9473a10c6cc48b88f1f4ff16155e6a9309f
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-02 21:59:33 +0530

    Add files via upload

---
+++ 02-02-2026/problem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int marks[5]= {80,90,85,70,95};
+
+    for (int i=0;i<0;i++) {
+        printf("students %d marks: %d\n",i+1,marks[i]);
+    }    
+    return 0;
+}
