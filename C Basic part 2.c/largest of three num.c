c5af340b395b796e5c817359f7938d71fc480e69
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-06 15:39:21 +0530

    Update largest of three num.c

--- C Basic part 2.c/largest of three num.c
+++ C Basic part 2.c/largest of three num.c
@@ -4,9 +4,9 @@ int main() {
 
     scanf("%d %d %d",&a,&b,&c);
 
-    if ((a > b) && (a > c)) {
+    if ((a > b) || (a > c)) {
         printf("%d",a);
-    } else if ((b > a) && (b > c)) {
+    } else if ((b > a) || (b > c)) {
         printf("%d",b);
     } else {
         printf("%d",c);
