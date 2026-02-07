a5617288aead438c5c88274e20043a5ff131a912
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-07 12:02:26 +0530

    Create comparision of case.c

---
+++ if else part 1.c/comparision of case.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+    char ch;
+
+    scanf("%c",&ch);
+
+    if (ch >= 65 && ch <= 90) {
+        printf("Uppercase");
+    } else {
+        printf("Lowercase");
+    }
+
+    return 0;
+}
