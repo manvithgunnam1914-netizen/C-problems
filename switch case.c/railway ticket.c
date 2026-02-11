72045903ee2286182588ff49581aa59dc577d21b
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-11 22:29:30 +0530

    Create railway ticket.c

---
+++ switch case.c/railway ticket.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int class,age;
+    float fare =  0;
+
+    scanf("%d %d",&class,&age);
+
+    switch(class) {
+        case 1:
+         fare = 300;
+
+         if (age < 12) {
+            fare = fare * 0.5;
+         } else if (age >= 60) {
+            fare = fare * 0.67;
+         }
+         break;
+
+         case 2:
+           fare = 1000;
+
+           if (age < 12) {
+            fare = fare * 0.5;
+           }
+           break;
+           default:
+           printf("Invalid");
+           return 0;
+    }   
+    printf("%0.f",fare);
+
+    return 0;
+}
