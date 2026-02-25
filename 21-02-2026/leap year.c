7d08bf3abe66cfc51359fbcc882930564b779f08
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-02-25 16:07:26 +0530

    Create leap year.c

---
+++ 21-02-2026/leap year.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int year;
+
+    scanf("%d",&year);
+
+    if (year % 400 == 0) {
+        printf("Century Leap Year");
+    }
+    else if (year % 100 == 0) {
+        printf("Not a Leap Year");
+    }
+    else if (year % 4 == 0) {
+        printf("Normal Leap Year");
+    }
+    else {
+        printf("Not a Leap Year");
+    }
+
+    return 0;
+}
