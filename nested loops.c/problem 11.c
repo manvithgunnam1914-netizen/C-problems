7ca95b499bba8a0e6b80886c051f1adf4a23293d
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-03-11 11:29:40 +0530

    Create problem 11.c

---
+++ nested loops.c/problem 11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int i,j,num;
+
+    for (i = 1; i <= 5; i++) {
+        if (i % 2 == 1){
+            num = 1;
+        } else {
+            num = 0;
+        }
+    
+    for (j = 1; j <= i; j++) {
+
+        printf("%d ",num);
+
+        if (num == 1) {
+            num = 0;
+        } else {
+            num = 1;
+        }
+    }
+    printf("\n");
+    }
+
+
+    return 0;
+}
