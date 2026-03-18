ad40332f8d418b9531a44d9a06c8effdb8793aa0
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-03-18 23:18:29 +0530

    Create sum of even numbers.c

---
+++ while loops.c/sum of even numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, i = 1, sum = 0;
+
+    scanf("%d",&n);
+
+    while(i <= n) {
+        if(i % 2 == 0) {
+            sum = sum + i;
+        }
+        i++;
+    }
+
+    printf("%d",sum);
+
+    return 0;
+}
