a6447e5ac6f0191785fc1a0911c4d10e1efa3201
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-04-07 22:51:55 +0530

    Create Problem 22.c

---
+++ Day 3/Problem 22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0, i = 1;
+
+    scanf("%d", &n);
+
+    while(i <= n) {
+        sum = sum + i;
+        i++;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
