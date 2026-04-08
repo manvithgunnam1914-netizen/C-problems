37d627aef2c6443a692de43ab0cbbdd779f767c8
manvithgunnam1914-netizen <manvithgunnam1914@gmail.com>
2026-04-08 22:54:48 +0530

    Create Problem 12.c

---
+++ Day 3/Problem 12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("%d\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
